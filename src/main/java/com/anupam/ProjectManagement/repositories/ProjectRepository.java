4a068f24d7446743f4128ba7a6077622148497d4
Anupam Guin <anupamguin7585@gmail.com>
2021-11-12 18:02:14 +0530

    Project structure made

---
+++ src/main/java/com/anupam/ProjectManagement/repositories/ProjectRepository.java
@@ -0,0 +1,13 @@
+package com.anupam.ProjectManagement.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.anupam.ProjectManagement.demo.Project;
+
+@Repository
+
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+	@Override
+	Iterable<Project> findAllById(Iterable<Long> iterable);
+}

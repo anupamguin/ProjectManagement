72b98129e2a03a6e862280f644d78fb9bdb08952
Anupam Guin <anupamguin7585@gmail.com>
2021-11-13 00:04:45 +0530

    Working on validation

--- src/main/java/com/anupam/ProjectManagement/demo/Project.java
+++ src/main/java/com/anupam/ProjectManagement/demo/Project.java
@@ -2,25 +2,45 @@ package com.anupam.ProjectManagement.demo;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 public class Project {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@NotBlank(message = "Project Name is Required")
 	private String projectName;
+	
+	@NotBlank(message = "Project Identifier is Required")
+	@Size(min = 4, max=5, message = "Please use 4 to 5 Characters")
+	@Column(updatable = false, unique = true)
 	private String projectIdentifier;
+	
+	@NotBlank(message = "Project description is Required")
 	private String description;
+	
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date start_date;
+	
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date end_date;
-
+	
+	@JsonFormat(pattern = "yyyy-mm-dd") 
 	private Date created_At;
+	
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date updated_At;
 
 	public Project() {
--- src/main/java/com/anupam/ProjectManagement/web/ProjectController.java
+++ src/main/java/com/anupam/ProjectManagement/web/ProjectController.java
@@ -1,8 +1,12 @@
 package com.anupam.ProjectManagement.web;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.StreamingHttpOutputMessage.Body;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,9 +23,13 @@ public class ProjectController {
 	private ProjectService projectService;
 	
 	@PostMapping("")
-	public ResponseEntity<Project> createNewProject(@RequestBody Project project)
-	{
-		Project project2 = projectService.saveOrUpdateProject(project);
-		return new ResponseEntity<Project>(project,HttpStatus.CREATED);
+	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult bindingResult)
+	{ /* if we are not use @Valid then for error input from postman console has error & BindingResult catch the
+	 	 error it has error then it show the message */
+		if(bindingResult.hasErrors())
+			return new ResponseEntity<String>("Invalid Project Object",HttpStatus.BAD_REQUEST);
+			
+		Project project1 = projectService.saveOrUpdateProject(project);
+		return new ResponseEntity<Project>(project1,HttpStatus.CREATED);
 	}
 }

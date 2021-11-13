dc7a10e6f370345bac31dac1e71dde91a784e214
Anupam Guin <anupamguin7585@gmail.com>
2021-11-13 23:49:05 +0530

    Validation for empty object

--- src/main/java/com/anupam/ProjectManagement/web/ProjectController.java
+++ src/main/java/com/anupam/ProjectManagement/web/ProjectController.java
@@ -1,11 +1,15 @@
 package com.anupam.ProjectManagement.web;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.StreamingHttpOutputMessage.Body;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,11 +28,21 @@ public class ProjectController {
 	
 	@PostMapping("")
 	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult bindingResult)
-	{ /* if we are not use @Valid then for error input from postman console has error & BindingResult catch the
-	 	 error it has error then it show the message */
-		if(bindingResult.hasErrors())
-			return new ResponseEntity<String>("Invalid Project Object",HttpStatus.BAD_REQUEST);
+	{ /* if we are not use @Valid then for error input from postman, in console has error & BindingResult catch the
+	 	 error, it has error then it show the message */
+		
+		if(bindingResult.hasErrors()) {
+		//	return new ResponseEntity<String>("Invalid Project Object",HttpStatus.BAD_REQUEST);
+		//	return new ResponseEntity<List<FieldError>>(bindingResult.getFieldErrors(),HttpStatus.BAD_REQUEST);
+		/* now for the above line ,	for empty object in postman body we get error */
 			
+			Map<String,String> errorMap=new HashMap<>();
+			for(FieldError error: bindingResult.getFieldErrors())
+			{
+				errorMap.put(error.getField(), error.getDefaultMessage());
+			}
+			return new ResponseEntity<Map<String,String>>(errorMap,HttpStatus.BAD_REQUEST);
+		}	
 		Project project1 = projectService.saveOrUpdateProject(project);
 		return new ResponseEntity<Project>(project1,HttpStatus.CREATED);
 	}

96a3c04ed679ff7341f56533f507ef0b511aa383
Anupam Guin <anupamguin7585@gmail.com>
2021-11-14 23:55:12 +0530

    created delete project api

--- src/main/java/com/anupam/ProjectManagement/services/ProjectService.java
+++ src/main/java/com/anupam/ProjectManagement/services/ProjectService.java
@@ -38,4 +38,13 @@ public class ProjectService {
 	{		
 		return projectRepository.findAll();	
 	}
+	
+	public void deleteProjectByIdentifier(String projectId)
+	{
+		Project project = projectRepository.findByProjectIdentifier(projectId);
+		if(project == null)
+			throw new ProjectIdException("Cannot Project with Id '"+projectId+"' .This Project does not exist");
+		
+		projectRepository.delete(project);
+	}
 }

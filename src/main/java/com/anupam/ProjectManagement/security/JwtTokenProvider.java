4ba42420d94cc7ab98cefe1582fc3c9c00d5fbce
Anupam <anupamguin7585@gmail.com>
2021-12-14 12:59:33 +0530

    Generated the token in JwtTokenProvider.java & make /login api

---
+++ src/main/java/com/anupam/ProjectManagement/security/JwtTokenProvider.java
@@ -0,0 +1,42 @@
+package com.anupam.ProjectManagement.security;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import com.anupam.ProjectManagement.demo.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import static com.anupam.ProjectManagement.security.SecurityConstants.EXPIRATION_TIME;
+import static com.anupam.ProjectManagement.security.SecurityConstants.SECRET;
+
+@Component
+public class JwtTokenProvider {
+	
+	// Generated the token
+	
+	public String generateToken(Authentication authentication) {
+		
+		User user=(User)authentication.getPrincipal();
+		Date now=new Date(System.currentTimeMillis());
+		Date expiryDate =new Date(now.getTime()+EXPIRATION_TIME);
+		
+		String userId=Long.toString(user.getId());
+		
+		Map<String,Object> claims = new HashMap<>();
+		claims.put("id", Long.toString(user.getId()));
+		claims.put("username", user.getUsername());
+		claims.put("fullname", user.getFullname());
+		
+		return Jwts.builder().setSubject(userId).setClaims(claims)
+				.setIssuedAt(expiryDate)
+				.signWith(SignatureAlgorithm.HS512, SECRET)
+				.compact();
+	}
+	// validate the token
+	
+	//Get user id from the token
+}
--- src/main/java/com/anupam/ProjectManagement/web/UserController.java
+++ src/main/java/com/anupam/ProjectManagement/web/UserController.java
@@ -5,6 +5,10 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,9 +16,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.anupam.ProjectManagement.demo.User;
+import com.anupam.ProjectManagement.payload.JWTLoginSuccessResponse;
+import com.anupam.ProjectManagement.payload.LoginRequest;
+import com.anupam.ProjectManagement.security.JwtTokenProvider;
 import com.anupam.ProjectManagement.services.MapValidationErrorService;
 import com.anupam.ProjectManagement.services.UserService;
 import com.anupam.ProjectManagement.validator.UserValidator;
+import static com.anupam.ProjectManagement.security.SecurityConstants.*;
 
 @RestController
 @RequestMapping("/api/users")
@@ -28,6 +36,12 @@ public class UserController {
 
 	@Autowired
 	UserValidator userValidator;
+	
+	@Autowired
+	private JwtTokenProvider jwtTokenProvider;
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
 
 	@PostMapping("/register")
 	public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult bindingResult){
@@ -40,5 +54,21 @@ public class UserController {
 		User newUser=userService.saveUser(user);
 
 		return new ResponseEntity<User>(newUser,HttpStatus.CREATED);
+	}
+	
+	@PostMapping("/login")
+	public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest,BindingResult bindingResult){
+		
+		ResponseEntity<?> errorMap =mapValidationErrorService.mapValidationService(bindingResult);
+		if(errorMap != null) return errorMap;
+		
+		Authentication authentication= authenticationManager.authenticate
+				(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+		
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		
+		String jwt =TOKEN_PREFIX + jwtTokenProvider.generateToken(authentication);
+		
+		return ResponseEntity.ok(new JWTLoginSuccessResponse(true, jwt));
 	}
 }

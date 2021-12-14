9f40d07b4639903f9cc5776dbfeeabbf65e67013
Anupam <anupamguin7585@gmail.com>
2021-12-14 15:08:34 +0530

    Add JwtAuthenticationFilter.java , validate token and get userId from token

--- src/main/java/com/anupam/ProjectManagement/security/JwtTokenProvider.java
+++ src/main/java/com/anupam/ProjectManagement/security/JwtTokenProvider.java
@@ -8,8 +8,15 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import com.anupam.ProjectManagement.demo.User;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
+
 import static com.anupam.ProjectManagement.security.SecurityConstants.EXPIRATION_TIME;
 import static com.anupam.ProjectManagement.security.SecurityConstants.SECRET;
 
@@ -36,7 +43,36 @@ public class JwtTokenProvider {
 				.signWith(SignatureAlgorithm.HS512, SECRET)
 				.compact();
 	}
+	
 	// validate the token
 	
+	public boolean validateToken(String token) {
+		try {
+			Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token);
+			return true;
+		} catch (SignatureException e) {
+			System.err.println("Invalid JWT Signature");
+		}
+		catch(MalformedJwtException e) {
+			System.err.println("Invalid JWT token");
+		}
+		catch(ExpiredJwtException e) {
+			System.err.println("Expired JWT token");
+		}
+		catch(UnsupportedJwtException e) {
+			System.err.println("Unsupported JWT token");
+		}
+		catch(IllegalArgumentException e) {
+			System.err.println("JWT claims String is empty");
+		}
+		return false;
+	}
+	
 	//Get user id from the token
+	
+	public Long getUserIdFromJWT(String token) {
+		Claims claims=Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token).getBody();
+		String id = (String)claims.get("id");
+		return Long.parseLong(id);
+	}
 }
--- src/main/java/com/anupam/ProjectManagement/security/SecurityConfig.java
+++ src/main/java/com/anupam/ProjectManagement/security/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.anupam.ProjectManagement.services.CustomUserDetailsService;
 
@@ -32,6 +33,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
 	
+	@Bean
+	public JwtAuthenticationFilter jwtAuthenticationFilter() {
+		return new JwtAuthenticationFilter();
+	}
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
 		authenticationManagerBuilder.userDetailsService(customUserDetailsService).passwordEncoder(bCryptPasswordEncoder);
@@ -66,6 +72,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers(SIGN_UP_URLS).permitAll()
 		.antMatchers(H2_URL).permitAll()
 		.anyRequest().authenticated();
+
+
+	  http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
 
 }
